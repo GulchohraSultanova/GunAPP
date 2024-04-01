5da6eb1540ab9f5ea9a88cdae0fe8c297a93cb93
GulchohraSultanova <GulchohraSultanova.com>
2024-04-01 15:54:26 +0400

    GunApp

---
+++ GunApp/Program1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunApp
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            Weapon weapon = new Weapon(10, 5, false);
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("-----------Welcome my App-----------");
+                Console.WriteLine();
+                Console.WriteLine("Menu:");
+                Console.WriteLine("0.Informasiya almaq ucun!");
+                Console.WriteLine("1.Shoot metodu ucun!");
+                Console.WriteLine("2.fire metodu ucun!");
+                Console.WriteLine("3.GetRemainBulletCount metodu ucun!");
+                Console.WriteLine("4.Reload metodu ucun!");
+                Console.WriteLine("5.ChangeFire metodu ucun!");
+                Console.WriteLine("6.Exit App....");
+                Console.WriteLine("7.Edit!");
+                Console.WriteLine();
+                Console.Write("Enter your choice: ");
+
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("Gulle tutumu: "+ weapon.BulletCapacity) ;
+                        Console.WriteLine("Gulle sayi:"+weapon.BulletCount);
+                        Console.Write("Gulle modu: ");
+                        if (!weapon.ShootingMode)
+                        {
+                            Console.WriteLine("Avtomatik");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tekli");
+                        }
+                        break;
+                    case "1":
+                       if(weapon.BulletCount >0) {
+                            weapon.Shoot();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Silahda gulle yoxdur");
+                        }
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        Console.WriteLine("Exiting the program...");
+                        flag = true;
+                        break;
+                    case "7":
+                        Console.WriteLine("8.Tutumu deyissin!");
+                        Console.WriteLine("9.Gulle sayi deyissin") ;
+                        Console.WriteLine() ;
+                        Console.WriteLine("Seciminizi edin: ");
+                        string s=Console.ReadLine();
+                        switch (s)
+                        {
+                            case "8":
+                                int capacity;
+                                do
+                                {
+                                    Console.Write("New capacity: ");
+                                }
+                                while (!int.TryParse(Console.ReadLine(), out capacity) ) ;
+                                weapon.BulletCapacity= capacity ;
+                                Console.WriteLine("tutum deyisdirildi!");
+                                break;
+                            case "9":
+                                int count;
+                                do
+                                {
+                                    Console.Write("New bullet count: ");
+                                }
+                                while (!int.TryParse(Console.ReadLine(), out count) || weapon.BulletCapacity<count);
+                                weapon.BulletCapacity = count;
+                                Console.WriteLine("Gulle sayi deyisdirildi!");
+                                break;
+                            default:
+                                Console.WriteLine("Duzgun secim etmediniz!");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("This is not correct! Please, enter correct choice!");
+                        break;
+                }
+            }
+            while (!flag);
+
+            
+    }
+    }
+}
+
---
+++ GunApp/Weapon.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunApp
+{
+    internal class Weapon
+    {
+        public int BulletCapacity { get; set; }
+        public int BulletCount { get; set; }
+        public bool ShootingMode{ get; set; }
+        public Weapon(int bulletCapacity,int bulletCount,bool shootingMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCount;
+            ShootingMode = shootingMode;
+        }
+        public void Shoot()
+        {
+            if(BulletCount >= 0)
+            {
+                BulletCount=BulletCount-1;
+                Console.WriteLine("Silah gulle atdi");
+            }
+            else
+            {
+
+                Console.WriteLine("Silahda gulle sayi sifirdir");
+              
+            }
+            
+            
+        }
+        public void Fire()
+        {
+            BulletCount = 0;
+            Console.WriteLine("Daraqdaki butun gulleler atelsendi"); ;
+        }
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity-BulletCount;
+        }
+        public void Reload()
+        {
+            BulletCount = BulletCapacity;
+            Console.WriteLine("Daraq yeniden doldu");
+        }
+        public void ChangeFireMode()
+        {
+            if(ShootingMode==false)
+            {
+
+                ShootingMode=true;
+                Console.WriteLine("Daragin atis modu tekliye cevrildi.");
+            }
+            else
+            {
+                ShootingMode = false;
+                Console.WriteLine("daragin atis modu avtomatike cevrildi"); ;
+            }
+        }
+
+
+
+    }
+}
